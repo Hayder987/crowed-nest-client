79d4e99ca93de28004ee6a5afcef1b1470185473
hayder <hayderbd4290@gmail.com>
2024-12-04 03:28:18 +0600

    Auth contex context Api created

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { createContext, useState } from "react";
+
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+
+
+    const authInfo = {
+      user, 
+      loading,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+          {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children:PropTypes.object
+}
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './router/router'
-import { RouterProvider } from 'react-router'
-import UtilitiesProvider from './Context/UtilitiesProvider'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./router/router";
+import { RouterProvider } from "react-router";
+import UtilitiesProvider from "./Context/UtilitiesProvider";
+import AuthProvider from "./Context/AuthProvider";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <UtilitiesProvider>
-     <RouterProvider router={router}></RouterProvider>
-   </UtilitiesProvider>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <UtilitiesProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </UtilitiesProvider>
+    </AuthProvider>
+  </StrictMode>
+);
